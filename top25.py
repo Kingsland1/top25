7dd03265a3b7a02af24518e213e1403b65fac8c6
liuchang <fantine16@163.com>
2017-09-02 21:56:02 +0800

    代码优化

--- top25.py
+++ top25.py
@@ -12,13 +12,12 @@
 
 import pandas as pd
 # import rolling_beta
-import numpy as np
 import matplotlib.pyplot as plt
 
 # writer = pd.ExcelWriter('test.xlsx')
 
 
-data = pd.DataFrame.from_csv("..\\top25\\ZZ500.csv")
+data = pd.DataFrame.from_csv("ZZ500.csv")
 columns=list(data.columns)[::7]
 data_open=data.iloc[1:,::7].astype(float)
 data_high=data.iloc[1:,1::7].astype(float)
@@ -30,24 +29,19 @@ data_high.columns=columns
 data_low.columns=columns
 data_close.columns=columns
 data_volume.columns=columns
-data_close=pd.DataFrame(data_close)
 return_close=data_close.pct_change()
 return_open=data_open.pct_change()
 return_intraday=(data_close-data_open)/data_open
 
 # 找到top25的股票的名字
-temp_top25={}
-for i,date in enumerate(return_close.index):
-    sorted_row_value=pd.Series(return_close.iloc[i]).sort_values(ascending=False)
-    sorted_row_name=sorted_row_value.index[:25]
-    temp_top25[date]=sorted_row_name
-top25=pd.DataFrame.from_dict(temp_top25,orient='index')
-# 找到top25股票名字 所对应的隔日回报，比如今天开盘买进，明天开盘卖出
-temp_top25_return_holding={}
-for i,date in enumerate(return_close.index[:-1]):
-    temp_top25_return_holding[date]=return_open.loc[return_open.index[i + 1], top25.iloc[i - 1, :]]# T0股票的收益，现实在return_open的T+1
+top25=pd.DataFrame(index=data_open.index,columns=range(25))
+for date in return_close.index:
+    top25.loc[date]=pd.Series(return_close.loc[date]).sort_values(ascending=False).index[:25]
 
-top25_return_holding=pd.DataFrame.from_dict(temp_top25_return_holding, orient='index')
+# 找到top25股票名字 所对应的隔日回报，比如今天开盘买进，明天开盘卖出
+top25_return_holding=pd.DataFrame(index=data_open.index,columns=columns )
+for i,date in enumerate(return_open.index[:-1]):
+     top25_return_holding.loc[date]=return_open.loc[return_open.index[i + 1], top25.iloc[i - 1, :]]
 # 把回报matrix row_wise 求平均，再累加起来
 top25_return_holding_avg=pd.DataFrame(top25_return_holding.mean(axis=1))
 top25_return_holding_avg_cumsum=pd.DataFrame(top25_return_holding_avg+1).cumprod()
