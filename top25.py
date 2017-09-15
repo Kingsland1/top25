52fc4f78578f807bc4a81be760da7b0a7582dbde
liuchang <fantine16@163.com>
2017-09-15 21:59:35 +0800

    添加3个控制参数

--- top25.py
+++ top25.py
@@ -16,27 +16,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def main(stock_num=25):
+def main(hold_days=1, stock_num=25, pick_window=1):
     """
     打板策略
     """
     data = pd.read_csv(filepath_or_buffer="ZZ500.csv", index_col=0, header=1)
     stock_columns = list(pd.read_csv(filepath_or_buffer="ZZ500.csv", index_col=0, nrows=1).columns)[::7]
-    date_index = data.index
     data_open = data.iloc[:, ::7].astype(float)
     data_close = data.iloc[:, 3::7].astype(float)
     data_open.columns = stock_columns
     data_close.columns = stock_columns
-    return_open = data_open.pct_change()
-    return_close = data_close.pct_change()
-    # 得到每天的选股，25个股票，第一天和第二天的闭盘收益比，得到第三天的选股
-    top_stocks = pd.DataFrame(index=date_index, columns=range(stock_num))
-    for i, date in enumerate(date_index[:-2]):
-        top_stocks.iloc[i+2] = pd.Series(return_close.iloc[i+1]).sort_values(ascending=False).index[:stock_num]
-    # 找到top25股票名字 所对应的隔日回报，比如今天开盘买进，明天开盘卖出
-    return_holding = pd.DataFrame(index=date_index, columns=range(stock_num))
-    for i, date in enumerate(date_index[2:-1]):
-        return_holding.loc[date] = pd.Series(return_open.loc[date_index[2:][i + 1], top_stocks.loc[date]].values)
+    top_stocks = get_stocks(data_close, stock_num, pick_window)
+    return_holding = get_return_holding(data_open, top_stocks, pick_window, hold_days, stock_num)
     # 把回报matrix row_wise 求平均，再计算累计积
     avg = return_holding.mean(axis=1)
     cumsum = (avg + 1).cumprod()
@@ -46,11 +37,39 @@ def main(stock_num=25):
     plt.show()
 
 
+def get_stocks(data_close, stock_num, pick_window):
+    """
+    得到每天的选股，stock_num个股票，第1天和第t天的闭盘收益比，得到第(t+1)天的选股
+    """
+    performance = data_close.pct_change(periods=pick_window)
+    date_index = data_close.index
+    top_stocks = pd.DataFrame(index=date_index, columns=range(stock_num))
+    for i in range(pick_window, len(date_index) - 1):
+        top_stocks.iloc[i + 1] = pd.Series(performance.iloc[i]).sort_values(ascending=False).index[:stock_num]
+    return top_stocks
+
+
+def get_return_holding(data_open, top_stocks, pick_window, hold_days, stock_num):
+    """
+    找到top25股票名字 所对应的隔日回报，比如今天开盘买进，明天开盘卖出
+    """
+    return_open = data_open.pct_change(periods=hold_days)
+    date_index = data_open.index
+    return_holding = pd.DataFrame(index=date_index, columns=range(stock_num))
+    for i in range(1 + pick_window, len(date_index) - hold_days):
+        date1 = date_index[i]
+        date2 = date_index[i + hold_days]
+        return_holding.loc[date2] = pd.Series(return_open.loc[date2, top_stocks.loc[date1]].values)
+
+    return return_holding
+
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        main(25)
-    elif len(sys.argv) == 2:
-        main(int(sys.argv[1]))
+        main()
+    elif len(sys.argv) == 4:
+        hold_days = int(sys.argv[1])
+        stock_num = int(sys.argv[2])
+        pick_window = int(sys.argv[3])
+        main(hold_days, stock_num, pick_window)
     else:
         print("args error", file=sys.stderr)
-
