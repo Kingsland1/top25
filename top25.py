ac459c0fb3ed4c6993386208854905e1ef994b69
liuchang <fantine16@163.com>
2017-09-15 22:32:22 +0800

    优化传递参数

--- top25.py
+++ top25.py
@@ -11,12 +11,12 @@ File: top25.py
 Author: Kingsland1(fantine16@163.com)
 Date: 2017/09/03 14:06:46
 """
-import sys
+import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def main(hold_days=1, stock_num=25, pick_window=1):
+def main(args):
     """
     打板策略
     """
@@ -26,8 +26,8 @@ def main(hold_days=1, stock_num=25, pick_window=1):
     data_close = data.iloc[:, 3::7].astype(float)
     data_open.columns = stock_columns
     data_close.columns = stock_columns
-    top_stocks = get_stocks(data_close, stock_num, pick_window)
-    return_holding = get_return_holding(data_open, top_stocks, pick_window, hold_days, stock_num)
+    top_stocks = get_stocks(data_close, args)
+    return_holding = get_return_holding(data_open, top_stocks, args)
     # 把回报matrix row_wise 求平均，再计算累计积
     avg = return_holding.mean(axis=1)
     cumsum = (avg + 1).cumprod()
@@ -37,10 +37,12 @@ def main(hold_days=1, stock_num=25, pick_window=1):
     plt.show()
 
 
-def get_stocks(data_close, stock_num, pick_window):
+def get_stocks(data_close, args):
     """
     得到每天的选股，stock_num个股票，第1天和第t天的闭盘收益比，得到第(t+1)天的选股
     """
+    stock_num = args.stock_num
+    pick_window = args.pick_window
     performance = data_close.pct_change(periods=pick_window)
     date_index = data_close.index
     top_stocks = pd.DataFrame(index=date_index, columns=range(stock_num))
@@ -49,10 +51,13 @@ def get_stocks(data_close, stock_num, pick_window):
     return top_stocks
 
 
-def get_return_holding(data_open, top_stocks, pick_window, hold_days, stock_num):
+def get_return_holding(data_open, top_stocks, args):
     """
     找到top25股票名字 所对应的隔日回报，比如今天开盘买进，明天开盘卖出
     """
+    stock_num = args.stock_num
+    pick_window = args.pick_window
+    hold_days = args.hold_days
     return_open = data_open.pct_change(periods=hold_days)
     date_index = data_open.index
     return_holding = pd.DataFrame(index=date_index, columns=range(stock_num))
@@ -64,12 +69,12 @@ def get_return_holding(data_open, top_stocks, pick_window, hold_days, stock_num)
     return return_holding
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        main()
-    elif len(sys.argv) == 4:
-        hold_days = int(sys.argv[1])
-        stock_num = int(sys.argv[2])
-        pick_window = int(sys.argv[3])
-        main(hold_days, stock_num, pick_window)
-    else:
-        print("args error", file=sys.stderr)
+    """
+    程序入口
+    """
+    parser = argparse.ArgumentParser(description='打板策略')
+    parser.add_argument('-hd', '--hold_days', type=int, default=1)
+    parser.add_argument('-sn', '--stock_num', type=int, default=25)
+    parser.add_argument('-pw', '--pick_window', type=int, default=1)
+    args = parser.parse_args()
+    main(args)
