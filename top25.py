188de141a55e7e3effd512d6340a582b12f72d21
raoraozu <769555056@qq.com>
2017-09-30 18:04:27 +0800

    添加temp_args作为临时参数
    添加temp_args Class
    添加图像的title

--- top25.py
+++ top25.py
@@ -16,6 +16,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+
 def main(args):
     """
     打板策略
@@ -29,35 +30,38 @@ def main(args):
     df1 = watch_stock(data_close, data_open, args)
     df2 = watch_hold_days(data_close, data_open, args)
     df3 = watch_pick_window(data_close, data_open, args)
-    df1.plot().get_figure().savefig('df1.png')
-    df2.plot().get_figure().savefig('df2.png')
-    df3.plot().get_figure().savefig('df3.png')
+    df1.plot(title=args).get_figure().savefig('df1.png')
+    df2.plot(title=args).get_figure().savefig('df2.png')
+    df3.plot(title=args).get_figure().savefig('df3.png')
+    plt.plot()
 
 
-def watch_stock(data_close, data_open, args, stock_range=range(15, 30, 3)):
+def watch_stock(data_close, data_open, args, stock_range=range(15, 50, 3)):
     """
     固定hold_days和pick_window，观察stock_num
     """
     date_index = data_open.index
     stock_range1 = ['stock_num=' + str(i) for i in stock_range]
     df = pd.DataFrame(index=date_index, columns=stock_range1)
+    temp_args = Temp_args(args)
     for i in stock_range:
-        args.stock_num = i
-        cumsum = strategy(data_close, data_open, args)
+        temp_args.stock_num = i
+        cumsum = strategy(data_close, data_open, temp_args)
         df['stock_num=' + str(i)] = cumsum
     return df
 
 
-def watch_hold_days(data_close, data_open, args, hold_range=range(1, 4)):
+def watch_hold_days(data_close, data_open, args, hold_range=range(1, 5)):
     """
     固定stock_num和pick_window，观察hold_days
     """
     date_index = data_open.index
     hold_range1 = ['hold_days=' + str(i) for i in hold_range]
     df = pd.DataFrame(index=date_index, columns=hold_range1)
+    temp_args = Temp_args(args)
     for i in hold_range:
-        args.hold_days = i
-        cumsum = strategy(data_close, data_open, args)
+        temp_args.hold_days = i
+        cumsum = strategy(data_close, data_open, temp_args)
         df['hold_days=' + str(i)] = cumsum
     return df
 
@@ -69,9 +73,10 @@ def watch_pick_window(data_close, data_open, args, pick_range=range(1, 4)):
     date_index = data_open.index
     pick_range1 = ['pick_window=' + str(i) for i in pick_range]
     df = pd.DataFrame(index=date_index, columns=pick_range1)
+    temp_args = Temp_args(args)
     for i in pick_range:
-        args.pick_window = i
-        cumsum = strategy(data_close, data_open, args)
+        temp_args.pick_window = i
+        cumsum = strategy(data_close, data_open, temp_args)
         df['pick_window=' + str(i)] = cumsum
     return df
 
@@ -117,13 +122,23 @@ def get_return_holding(data_open, top_stocks, args):
         return_holding.loc[date2] = pd.Series(return_open.loc[date2, top_stocks.loc[date1]].values)
     return return_holding
 
+
+
+
 if __name__ == "__main__":
     """
     程序入口
     """
     parser = argparse.ArgumentParser(description='打板策略')
-    parser.add_argument('-hd', '--hold_days', type=int, default=1)
-    parser.add_argument('-sn', '--stock_num', type=int, default=25)
+    parser.add_argument('-hd', '--hold_days', type=int, default=3)
+    parser.add_argument('-sn', '--stock_num', type=int, default=15)
     parser.add_argument('-pw', '--pick_window', type=int, default=1)
     args = parser.parse_args()
+    class Temp_args:
+        def __init__(self,args):
+            self.hold_days = args.hold_days
+            self.stock_num = args.stock_num
+            self.pick_window = args.pick_window
     main(args)
+
+
